da8965aa230e34732fabe5e2051d4dbc9f8fa479
camillifidelis <cfidelisg@gmail.com>
2026-01-20 20:11:49 -0300

    Obtendo um usuario por id

--- src/main/java/com/camillifidelis/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/camillifidelis/workshopmongo/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.camillifidelis.workshopmongo.repository;
 
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.camillifidelis.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findById(String id);
 
 }
--- src/main/java/com/camillifidelis/workshopmongo/services/UserService.java
+++ src/main/java/com/camillifidelis/workshopmongo/services/UserService.java
@@ -1,11 +1,11 @@
 package com.camillifidelis.workshopmongo.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.camillifidelis.workshopmongo.domain.User;
 import com.camillifidelis.workshopmongo.repository.UserRepository;
+import com.camillifidelis.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +15,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        return user;
     }
 }
